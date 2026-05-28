3aa02a40d30de59ef3fad3b52d50c500612c0416
MelyGM <133719381+MelyGM@users.noreply.github.com>
2026-05-27 17:52:26 -0700

    Implementacion del estado inicial, acciones legales  y es terminal

--- blackjack.py
+++ blackjack.py
@@ -15,36 +15,90 @@ class BlackJack(MDPsim):
     
     """
     def __init__(self, gama):
-        # TODO: definir los parámetros del blackjack, agrega lo que consideres necesario
-        self.estados = None # TODO: definir los estados del blackjack
+        self.estados = []
         self.gama = gama
+
+        self.estados = [
+            (suma, carta, as_usable)
+            for suma in range(12, 22)
+            for carta in range(1,11)
+            for as_usable in [True, False]
+        ]
+
+        self.suma_crupier =0
+        self.as_crupier =False
+
+    def reparte_carta(self):
+        carta = randint(1, 13)
+
+        if carta > 10:
+            return 10
+        else:
+            return carta
         
     def estado_inicial(self):
-        # TODO: implementar el estado inicial del blackjack
-        raise NotImplementedError("Implementa el estado inicial del blackjack")
+        #jugador 1
+        carta1 = self.reparte_carta()
+        carta2 = self.reparte_carta()
+        suma_jugador = carta1 + carta2
+
+        as_usable = False
+        if 1 in [carta1, carta2]:
+            if suma + 10 <= 21:
+                suma += 10
+                as_usable = True
+
+
+        #crupier
+        carta_crupier1 = self.reparte_carta()
+        carta_crupier2 = self.reparte_carta()
+        self.suma_crupier = carta_crupier1 + carta_crupier2
+
+        carta_visbile = carta_crupier1
+
+        self.as_crupier = False
+        if 1 in [carta_crupier1, carta_crupier2]:
+            if self.suma_crupier + 10 <= 21:
+                self.suma_crupier += 10
+                self.as_crupier = True
+
+        #blackjack natural
+        if suma_jugador == 21:
+            return ("Blackjack",1.5)
+        
+        #si j1 tiene menos de 12
+        while suma_jugador < 12:
+            nueva=self.reparte_carta()
+            suma_jugador += nueva
+        return (suma_jugador, carta_visbile, as_usable)
     
     def acciones_legales(self, s):
-        # TODO: implementar las acciones legales del blackjack
-        raise NotImplementedError("Implementa las acciones legales del blackjack")
-    
+        if self.es_terminal(s):
+            return []
+        else:
+            return [0, 1]
+      
     def recompensa(self, s, a, s_):
         # TODO: implementar la recompensa del blackjack
         raise NotImplementedError("Implementa la recompensa del blackjack")
     
     def transicion(self, s, a):
         # TODO: implementar la transición del blackjack
-        raise NotImplementedError("Implementa la transición del blackjack")
+        raise NotImplementedError("Implementa la transición del blackjack") 
     
     def es_terminal(self, s):
-        # TODO: implementar la condición de estado terminal del blackjack
-        raise NotImplementedError("Implementa la condición de estado terminal del blackjack")
+        return type(s) == tuple  and len(s) == 2 and s[0] == "Blackjack"
 
 
 if __name__ == "__main__":
 
-    blackjack = BlackJack(gama=1,...) # TODO: agregar los parámetros necesarios para el blackjack   
-
-    # TODO: definir los parámetros de SARSA y Q-learning, luego crear las instancias 
+    blackjack = BlackJack(gama=1) # TODO: agregar los parámetros necesarios para el blackjack   
+    s = blackjack.estado_inicial()
+    print("Estado inicial:", s)
+    print("Acciones legales:", blackjack.acciones_legales(s))
+    print ("terminaaaaaal? ", blackjack.es_terminal(s)) 
+    """
+    # definir los parámetros de SARSA y Q-learning, luego crear las instancias 
     # de cada algoritmo
     Q_sarsa = SARSA( blackjack, alfa=..., epsilon=..., n_ep=..., n_iter=...)
     Q_learning = Q_learning( blackjack, alfa=..., epsilon=..., n_ep=..., n_iter=...)
@@ -62,7 +116,7 @@ if __name__ == "__main__":
                   + str(pi_s(s)).center(10) + '|' 
                   + str(pi_q(s)).center(10))
     print("-"*10 + '|' + "-"*10 + '|' + "-"*10)
-
+"""
 
 """
 ****************************************************************************************
